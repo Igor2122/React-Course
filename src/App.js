fff8f4194d0a084914abc0b5de97c06e090c4794
Igor <igor_pavlenko@yahoo.com>
2018-11-15 21:39:58 +0300

    full interection with the list items updtating them in modern fast way

--- src/App.js
+++ src/App.js
@@ -6,37 +6,47 @@ class App extends Component {
 
   state = {
     persons: [
-      {name: 'Igor', age: 32},
-      {name: 'Reeem', age: 26},
-      {name: 'Stephanie', age: 28},
+      {id: 1, name: 'Igor', age: 32},
+      {id: 2, name: 'Reeem', age: 26},
+      {id: 3, name: 'Stephanie', age: 28},
     ],
       showPersons: false
   }
 
-  deletePersonHandler = (peronIndex) => {
+  deletePersonHandler = (personIndex) => {
     // traditional
     // const persons = this.state.persons.slice();//copies the full array and returns & returns a new which is stored here
     // or spread operator: 
     const persons = [...this.state.persons]; // new array from the old array objects
-    persons.splice(peronIndex, 1);// removes 1 element from the array
+    persons.splice(personIndex, 1);// removes 1 element from the array
     this.setState({persons: persons}) // we set it back with removed element
+    console.log(personIndex);
   }
   
 
-  nameChangedHandler = (event) => {
-    this.setState({persons: [
-      {name: 'Igor', age: 32},
-      {name: event.target.value, age: 25},
-      {name: 'Archie', age: 2}
-    ]}); 
-  }
 
   togglePersonHandler = () => {
     const doesShow = this.state.showPersons;// does show is bool false
     this.setState({showPersons: !doesShow});// assign opposite to the above to toggle
   }
 
-  
+  nameChangedHandler = (event, id) => {
+    // find the person we updating 
+    const personIndex = this.state.persons.findIndex(
+      prsn => {
+        return prsn.id === id;//True/False compare to the id we got from the method on the button
+      }
+    );// as map() will execute on evety element of the array
+    const person = {
+      ...this.state.persons[personIndex]
+    }// we are getting the id of the person we have found 
+
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons});
+  }  
 
   render() {
 
@@ -58,9 +68,12 @@ class App extends Component {
         // convert this array to valid jsx & reder the array with map()
         // will exc a method on the given arr
            return <Person 
+                  id={person.id}
                   name={person.name}
                   age={person.age}
-                  click={() => this.deletePersonHandler(index)} /> // return jsx
+                  click={() => this.deletePersonHandler(index)} 
+                  key={person.id}
+                  changed={(event) => this.nameChangedHandler(event,person.id)}/> // return jsx
         })} 
           <h1>Hi I am a React App</h1>
           <p>This is really working</p>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -8,7 +8,7 @@ const person = (props) => {
          <p 
          onClick={props.click} 
          onMouseEnter={props.mousEnt}
-         >I'am {props.name} and I am {props.age} years old!</p>
+         >#:{props.id} I'am {props.name} and I am {props.age} years old!</p>
          <p>{props.children}</p>
          <input type="text" onChange={props.changed} value={props.name}></input>
       </div>

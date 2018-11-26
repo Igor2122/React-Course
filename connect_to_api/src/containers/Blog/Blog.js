b5d204040fc8d219b3d9e982aa0529dad358ad9e
Igor <igor_pavlenko@yahoo.com>
2018-11-26 17:13:42 +0000

    save before node issue

--- connect_to_api/src/containers/Blog/Blog.js
+++ connect_to_api/src/containers/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // import axios from 'axios';
 // import axios from '../../axios';
 
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost'
@@ -18,12 +18,12 @@ class Blog extends Component {
             <header>
                 <nav>
                     <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to={{
-                            pathname: "new-post",
+                        <li><NavLink to="/">Home</NavLink></li>
+                        <li><NavLink to={{
+                            pathname:  "/new-post", // relative path(takes the cur path + new) 
                             hash: '#submit', // examples
                             search: '?quick-submit=true' // examples
-                        }}>New Post</Link></li>
+                        }}>New Post</NavLink></li>
                     </ul>
                 </nav>  
             </header>

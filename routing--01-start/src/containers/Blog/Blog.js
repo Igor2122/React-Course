b0dc075b7940302fd59997727ca60fce31922730
Igor <igor_pavlenko@yahoo.com>
2018-12-01 13:45:07 +0000

    routing restructure

---
+++ routing--01-start/src/containers/Blog/Blog.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+// import axios from 'axios';
+import axios from '../../axios';
+import './Blog.css';
+import { Route, NavLink, Switch } from 'react-router-dom';
+
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
+
+class Blog extends Component {
+
+
+    
+    render () {
+        
+
+        return (
+            <div>
+                <header className="Blog">
+                    <nav>
+                        <ul>
+                            <li>
+                                <NavLink 
+                                exact 
+                                to="/"
+                                activeClassName="myActive"
+                                >Home</NavLink>
+                            </li>
+                            <li><NavLink to={{
+                                pathname: "/new-post", 
+                                // pathname: this.props.match.url + "/new-post", // this will be a relative path 
+                                hash: '#submit',
+                                search: '?quick-submit=true'/*some examples*/
+                            }}>New Post</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+                
+                {/* rendering pure JSX
+                <Route path="/" exact render={() => <h1>Home</h1>}/>
+                <Route path="/" render={() => <h1>Home</h1>}/> */}
+                <Switch>
+                    <Route path="/" exact component={Posts} />
+                    <Route path="/new-post"  component={NewPost} />
+                    <Route path="/:postId" exact component={FullPost} />
+                </Switch>
+                
+            </div>
+        );
+    }
+}
+
+export default Blog;

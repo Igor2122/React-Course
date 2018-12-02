eb10bad91b2a8b52c1c09ebd7e928957602f9cdb
Igor <igor_pavlenko@yahoo.com>
2018-12-02 11:51:01 +0000

    lazy loading added to create new post page

--- routing--01-start/src/containers/Blog/Blog.js
+++ routing--01-start/src/containers/Blog/Blog.js
@@ -5,13 +5,18 @@ import './Blog.css';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
 import FullPost from './FullPost/FullPost';
 
+// import NewPost from './NewPost/NewPost';
+import assyncComponent from '../../hoc/assyncComp';
+const AsyncNewPost  = assyncComponent(() => {
+    return import('./NewPost/NewPost');
+})
+ 
 class Blog extends Component {
     
     state = {
-        auth: false,
+        auth: true,
     }
     
     
@@ -21,7 +26,7 @@ class Blog extends Component {
         
         let newPost = null;
         if(this.state.auth){
-            newPost = <Route path="/new-post"  component={NewPost} />
+            newPost = <Route path="/new-post"  component={AsyncNewPost} />
         }
 
         return (
@@ -52,7 +57,8 @@ class Blog extends Component {
                 <Switch>
                     
                     <Route path="/posts"  component={Posts} />
-                    <Redirect from="/" to="/posts" />
+                    {/*<Route render={() => <h1>Page Not Found</h1>}/>*/}
+                    {/*<Redirect from="/" to="/posts" /> */}
                 </Switch>
                 {newPost}
             </div>

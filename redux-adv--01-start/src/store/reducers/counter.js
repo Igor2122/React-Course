3b33a52e3cec4d6b67a6a0a24c892472660147ac
Igor <igor_pavlenko@yahoo.com>
2018-12-17 10:22:10 +0300

    added utility funtion to updated the state imutably

--- redux-adv--01-start/src/store/reducers/counter.js
+++ redux-adv--01-start/src/store/reducers/counter.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionsTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     counter: 0
@@ -8,24 +9,29 @@ const reducer = ( state = initialState, action ) => {
     // eslint-disable-next-line
     switch ( action.type ) {
         case actionTypes.INCREMENT:
-            const newState = Object.assign({}, state);
-            newState.counter = state.counter + 1;
-            return newState;
+            // const newState = Object.assign({}, state); - long version of updating the state
+            // newState.counter = state.counter + 1;
+            // return newState;
+            return updateObject(state, {counter: state.counter + 1})
         case actionTypes.DECREMENT:
-            return {
-                ...state,
-                counter: state.counter - 1
-            }
+            return updateObject(state, {counter: state.counter - 1});
+            // return {
+            //     ...state,
+            //     counter: state.counter - 1
+            // }
         case actionTypes.ADD:
-            return {
-                ...state,
-                counter: state.counter + action.val
-            }
+            return updateObject(state, {counter: state.counter + action.val});
+            // return {
+            //     ...state,
+            //     counter: state.counter + action.val
+            // }
         case actionTypes.SUBTRACT:
-            return {
-                ...state,
-                counter: state.counter - action.val
-            }
+            // alternative way of doing this is to use the utility function we created so we do not have to repeat the same code over 
+            return updateObject(state, {counter: state.counter - action.val});
+            // return {
+            //     ...state,
+            //     counter: state.counter - action.val
+            // }
     }
     return state;
 };
--- redux-adv--01-start/src/store/reducers/result.js
+++ redux-adv--01-start/src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionsTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     results: []
@@ -9,19 +10,21 @@ const reducer = ( state = initialState, action ) => {
     switch ( action.type ) {
         case actionTypes.STORE_RESULT:
         // here we can add some logic !considered to be a better practice
-            return {
-                ...state,
-                results: state.results.concat({id: new Date(), value: action.result * 2})
-            }
+            // return {
+            //     ...state,
+            //     results: state.results.concat({id: new Date(), value: action.result * 2})
+            // }
+            return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result * 2})})
         case actionTypes.DELETE_RESULT:
             // const id = 2;
             // const newArray = [...state.results];
             // newArray.splice(id, 1)
             const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return {
-                ...state,
-                results: updatedArray
-            }
+            return updateObject(state, {results: updatedArray});
+            // return {
+            //     ...state,
+            //     results: updatedArray
+            // }
     }
     return state;
 };
